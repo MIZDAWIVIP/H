b61f4caeb5cc33f1b881c80f44fcfaf65680033f
MIZDAWIVIP <mizdawivip@gmail.com>
2024-08-08 22:19:44 +0200

    Add files via upload

---
+++ اداتي تتبيت اساسيات.py
@@ -0,0 +1,16 @@
+import sys
+
+import os
+
+os.system("apt update") 
+os.system("apt upgrade") 
+os.system("pkg install python") 
+os.system("pkg install git nodejs") 
+os.system("pip install requests") 
+os.system("pip install mechanize") 
+os.system("pip install lolcat") 
+os.system("pip install bs4") 
+os.system("pkg install python") 
+os.system("pip install rich") 
+#_________________________________________#
+#اداه المزداوي ل تتبيت الاساسيات 
